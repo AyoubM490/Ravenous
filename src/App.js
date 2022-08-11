4b5c09ca60555d936e3deb4a5a8e39df83174218
AyoubMS <chahayb563@gmail.com>
2022-08-11 23:49:01 +0100

    use props for communication between components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import "./App.css";
 import SearchBar from "./components/SearchBar/SearchBar";
 import BusinessList from "./components/BusinessList/BusinessList";
 
+const business = {
+  imageSrc:
+    "https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg",
+  name: "MarginOtto Pizzeria",
+  address: "1010 Paddington Way",
+  city: "Flavortown",
+  state: "NY",
+  zipCode: "10101",
+  category: "Italian",
+  rating: 4.5,
+  reviewCount: 90,
+};
+
+const businesses = [business, business, business, business, business, business];
+
 function App() {
   return (
     <div class="App">
       <h1>ravenous</h1>
       <SearchBar />
-      <BusinessList />
+      <BusinessList businesses={businesses} />
     </div>
   );
 }
